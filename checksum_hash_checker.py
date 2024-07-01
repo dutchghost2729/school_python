efb08c6f86d3e8f88bf965d9e33a36ca9a39eb20
dutchghost2729 <bcamp111863@gmail.com>
2024-07-01 17:16:23 -0500

    Add files via upload
    copy and paste the hashes for easy comparison

---
+++ checksum_hash_checker.py
@@ -0,0 +1,11 @@
+# checksum hash checker
+
+first_hash = input("First hash:\n")
+second_hash = input("Second hash:\n")
+
+if first_hash == second_hash:
+    print("All good, hashes match.")
+    
+else:
+    print("\nWARNING\nHashes do not match!")
+    
